5679a71da0c6322afb0dbd6d07b0b4ef0e6f53c5
Matheus Pedroso <matheus_pedroso2012@hotmail.com>
2025-07-23 20:23:53 -0300

    Add project files.

---
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context;
+
+public class MySQLContext(DbContextOptions<MySQLContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
---
+++ GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,48 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            ConfigureServices(builder);
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+        // This method is used to configure services customize for the application.
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+            builder.Services.AddDbContext<MySQLContext>(options =>
+            {
+                options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 4)));
+            });
+        }
+    }
+}
